061cec6524aa0e63d2a04356cc0e0249f0f50732
valrichter <valenrichter11@gmail.com>
2023-03-12 17:05:22 -0300

    Start

---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.paths = {};
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicacion
+    this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+
+    // Directorio publico
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    //this.app.use(this.paths.auth, require("../routes/auth"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server on port ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
