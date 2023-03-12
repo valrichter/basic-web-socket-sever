ad7e81645bbca5bdae7389be918e440c35c0efcc
valrichter <valenrichter11@gmail.com>
2023-03-12 17:37:51 -0300

    Add sockets conections

--- models/server.js
+++ models/server.js
@@ -5,6 +5,8 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.server = require("http").createServer(this.app);
+    this.io = require("socket.io")(this.server);
     this.paths = {};
 
     // Middlewares
@@ -12,6 +14,9 @@ class Server {
 
     // Rutas de mi aplicacion
     this.routes();
+
+    // Sockets
+    this.sockets();
   }
 
   async conectarDB() {
@@ -30,8 +35,17 @@ class Server {
     //this.app.use(this.paths.auth, require("../routes/auth"));
   }
 
+  sockets() {
+    this.io.on("connection", (socket) => {
+      console.log("Connection client", socket.id);
+      socket.on("disconnect", () => {
+        console.log("Desconection client", socket.id);
+      });
+    });
+  }
+
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log(`Server on port ${this.port}`);
     });
   }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,17 @@
-console.log("Hola");
+// Referencias del HTML
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+
+const socket = io();
+
+socket.on("connect", () => {
+  console.log("Connect");
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("Disconnect");
+  lblOnline.style.display = "none";
+  lblOffline.style.display = "";
+});
